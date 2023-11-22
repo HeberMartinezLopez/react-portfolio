88e3b6a91cbb13f1080621daa1014c551d5f61f2
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-22 14:36:33 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouteProvider } from 'react-router-dom'
+
+import Root from './components/Root'
+import About from './components/About'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
+import Work from './components/Work'
+import Footer from './components/Footer'
+
+function App (){
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root/>}>
+        <Route index element={<About/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/work' element={<Work/>}/>
+      </Route>
+    )
+  )
+
+  return(
+    <>
+    <RouteProvider router={router}/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
