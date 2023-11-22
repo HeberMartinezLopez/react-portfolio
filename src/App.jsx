4e0493c49a37393aea45ce86b29d0315aa87d71d
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-22 15:19:14 -0800

    add index.jsx and root.css to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouteProvider } from 'react-router-dom'
-
-import Root from './components/Root'
-import About from './components/About'
-import Gallery from './components/Gallery'
-import Contact from './components/Contact'
-import Work from './components/Work'
-import Footer from './components/Footer'
+import { createBrowserRouter, createRoutesFromElements, Route, RouteProvider } from 'react-router-dom';
+import Root from './components/Root';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Work from './components/Work';
+import Footer from './components/Footer';
 
 function App (){
 
