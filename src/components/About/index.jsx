7ea4abad5e2347cff3d4040e1788f91f8d493a55
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-25 13:14:24 -0800

    add jsx and css to about component

---
+++ src/components/About/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <div className='img-container'>
+                <img src='' alt=''/>
+            </div>
+            <section className='about-me-content'>
+                <p>Hello! My name is Heber Martinez Lopez, a Junior Developer and a multi-musician based in the beautiful Bay Area. </p>
+                <p>Creating different apps is a passion of mine that is an art for me. I take pride...</p>
+                <p>When I'm not coding or build some app, I am preforming or recording music for ...</p>
+                <p>...</p>
+                <p>
+                    Cheers,
+                    <br/>
+                    Heber
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default About;
